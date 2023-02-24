ed43f2c669cb356cd55c0cebd3ad4281d1f556d4
simon <simonjzhou@gmail.com>
2023-02-23 17:24:32 -0800

    Add: finished slack cron job

--- src/pages/api/cron/[cron].ts
+++ src/pages/api/cron/[cron].ts
@@ -1,16 +1,69 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextApiRequest, NextApiResponse } from 'next';
+import arxiv from 'arxiv-api';
+import moment from 'moment';
 
-export const config = {
-  runtime: 'edge',
+type Papers = {
+  id: string
+  title: string
+  summary: string
+  published: string
 }
 
-export default async function handler(req: NextRequest) {
-  const cron = req.nextUrl.searchParams.get('cron')
-  if (!cron) return new Response('No cron provided', { status: 400 })
-  const response = {
-    interval: cron
-  }
-  return new NextResponse(JSON.stringify(response), {
-    status: 200,
+export const config = {
+  runtime: 'nodejs',
+}
+
+function constructSlackPayload(papers: Papers[]) {
+  const papersPayload = papers.map(({ id, title, summary, published }) => {
+    const paper = {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `*${title}*\n\n${summary}\n\nPublished on: ${moment(published.split('T')[0], "YYYY-MM-DD").format('MMMM Do, YYYY')}\n${id}`
+      },
+    }
+
+    return paper
   })
+
+  const completePayload = {
+    "blocks": [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `*arXiv digest for ${moment().format('MMMM Do, YYYY')}*`
+        }
+      },
+      {
+        "type": "divider"
+      },
+      ...papersPayload
+    ]
+  }
+  return completePayload
+}
+
+
+export default async function handler(_: NextApiRequest, res: NextApiResponse) {
+  const papers = await arxiv.search({
+    searchQueryParams: [
+      {
+        include: [{ name: 'LLM' }],
+      }
+    ],
+    start: 0,
+    maxResults: 5,
+    sortBy: 'submittedDate',
+    sortOrder: 'descending'
+  });
+
+  const response = await fetch(process.env.SLACK_WEBHOOK_URL ?? '', {
+    method: 'POST',
+    body: JSON.stringify(constructSlackPayload(papers))
+  })
+
+  res.status(200).json({
+    body: response
+  });
 }
